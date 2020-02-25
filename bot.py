8c4f0a79793ddf755c9598404df21c912faee1bd
surfordie <connormcgowen@gmail.com>
2020-02-24 21:53:34 -0800

    Sends one reponse instead of indivdual messages

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from mcstatus import MinecraftServer
 import discord
 import json
-from socket import timeout
+from socket import timeout, setdefaulttimeout
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,24 +20,26 @@ async def on_message(message):
         return
 
     if message.content == '!mcserver': 
+        server_status = []
         for hostname in hostnames:
 
             try:
                 server = MinecraftServer.lookup(hostname)
                 query = server.query()
                 status = server.status()
-                if query.players.names == []:
+                if query.players.names:
                     response = (f"{hostname} is online with {query.players.online}"
                     f" players and a latency of {status.latency}ms")
                 else:
-                    response = (f"{hostname} is online with {query.players.online}: "
+                    response = (f"{hostname} is online with {query.players.online} players: "
                     f"{query.players.names} and a latency: {status.latency}ms")
             except timeout:
-                response = f"{hostname} is offline"
+                response = f"{hostname} is offline (Timed out)"
 
             except Exception as e:
                 response = f"bot encountered exception {e}"
 
-            await message.channel.send(response)
-
+            server_status.append(response)
+        total = f"{server_status[0]}\n{server_status[1]}\n{server_status[2]}"
+        await message.channel.send(total)
 client.run(token)
