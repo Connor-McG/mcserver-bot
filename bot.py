ede51e26b77f2b721251e5e46f6241e18ef7a1a5
Surfordie <connor.mcgowen@yahoo.com>
2020-03-29 16:55:25 -0700

    Formated response, lists all player names

--- bot.py
+++ bot.py
@@ -11,6 +11,30 @@ GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 hostnames = json.loads(os.environ['MC_HOSTNAMES'])
 
+def is_online(server):
+    try:
+        ping_response = server.ping()
+        return ping_response
+    except:
+        return False
+
+def server_info(server):
+    data = {}
+    status_response = server.status(retries=1)
+
+    data['version'] = status_response.version.name
+    data['protocol'] = status_response.version.protocol
+    data['motd'] = status_response.description['text']
+    data['player_count'] = status_response.players.online
+    data['player_max'] = status_response.players.max
+    data['players'] = []
+
+    if status_response.players.sample is not None:
+        data['players'] = [{'name': player.name, 'id': player.id} for player in
+                           status_response.players.sample]
+
+    return data
+
 @client.event
 async def on_ready():
     for guild in client.guilds:
@@ -26,39 +50,25 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == '!mcserver':
+    if message == '!mcserver':
         for hostname in hostnames:
             server = MinecraftServer.lookup(hostname)
-            data = {'online': 'offline'}
-            try:
-                ping_response = server.ping()
-                data['online'] = 'online'
-                data['ping'] = ping_response
 
-                status_response = server.status(retries=1)
-                data['version'] = status_response.version.name
-                data['protocol'] = status_response.version.protocol
-                data['motd'] = status_response.description
-                data['player_count'] = status_response.players.online
-                data['player_max'] = status_response.players.max
-                data['players'] = []
-                if status_response.players.sample is not None:
-                    data['players'] = [{'name': player.name, 'id': player.id} for player in status_response.players.sample]
+            if is_online(server):
+                data = server_info(server)
 
-                query_response = server.query(retries=1)
-                data['host_ip'] = query_response.raw['hostip']
-                data['host_port'] = query_response.raw['hostport']
-                data['map'] = query_response.map
-                data['plugins'] = query_response.software.plugins
-            except:
-                pass
-            if data['online'] == 'offline':
-                response = f"{hostname}: \U0000274C"
+                response = f"```{hostname} \U00002705 --- ver {data['version']} \n" \
+                           f"   {data['motd']} \n " \
+                           f"   Players ({data['player_count']}/{data['player_max']}) \n" \
+\
+                        for player in data['players']:
+                            response += f'      - {player["name"]}'
+
+                response += ' ```'
+
             else:
-                if data['players']:
-                    response = f"{hostname}: \U00002705 - version: {data['version']} players: {data['player_count']}/{data['player_max']} {data['players']} ping: {data['ping']}ms motd: {data['motd']['text']}"
-                else: 
-                    response = f"{hostname}: \U00002705 - version: {data['version']} players: {data['player_count']}/{data['player_max']} ping: {data['ping']}ms motd: {data['motd']['text']}"
+                response =  f"``` **{hostname}**  \U0000274C ``` "
+
             await message.channel.send(response)
         
         
